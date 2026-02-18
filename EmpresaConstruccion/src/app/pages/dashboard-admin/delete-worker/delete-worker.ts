6f003e57e90715251be386e2eeaff493ab11970d
andreaheredia99 <andrea.heredia99@gmail.com>
2026-02-18 18:55:04 +0100

    Delete fucnionando

--- EmpresaConstruccion/src/app/pages/dashboard-admin/delete-worker/delete-worker.ts
+++ EmpresaConstruccion/src/app/pages/dashboard-admin/delete-worker/delete-worker.ts
@@ -1,4 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { IUser } from './../../../interfaces/iuser';
+import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../../core/services/users-service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -12,42 +13,60 @@ import Swal from 'sweetalert2';
 })
 export class DeleteWorker {
   router = inject(Router);
-  route = inject(ActivatedRoute);
+  //route = inject(ActivatedRoute);
   userService = inject(UserService);
 
-  id_users!: string;
-  
+  // lista usuarios
+  ArrayUsers = signal<IUser[]>([]);
+  // usuario seleccionado
+  selectedUser = signal<IUser | null>(null);
+
 
   form: FormGroup = new FormGroup({
-    name: new FormControl(),
-    surname: new FormControl(),
-    mail: new FormControl(),
-    role: new FormControl(),
+    selectedUser: new FormControl()
   });
 
+
   async ngOnInit() {
-    this.id_users = this.route.snapshot.paramMap.get('id_users')!;
-    // obtener id desde URL
 
     try {
-      const worker = await this.userService.getById(this.id_users);
-      // Rellenamos el formulario con los datos del trabajador para ver a quién borramos
-      this.form.patchValue(worker);
 
-      // Deshabilitamos el formulario para que sea solo de lectura (opcional pero recomendado)
-      this.form.disable();
+      const workers = await this.userService.getAll();
+
+      this.ArrayUsers.set(workers);
+
     } catch (error) {
+
       Swal.fire({
         title: 'Mistake!',
-        text: 'Could not load worker data',
+        text: 'Could not load workers',
         icon: 'error',
         confirmButtonText: 'Ok',
       });
+
     }
+
+  }
+
+
+  async onUserSelect(event: any) {
+
+    const id = Number(event.target.value);
+
+    const worker = this.ArrayUsers().find(
+      user => user.id_users === id
+    );
+
+    if (worker) {
+      this.selectedUser.set(worker);
+    }
+
   }
 
   async onSubmit() {
-    // 1. Preguntar confirmación antes de borrar
+
+    if (!this.selectedUser()) return;
+
     const result = await Swal.fire({
       title: 'Are you sure?',
       text: 'You are about to deactivate this worker!',
@@ -59,8 +78,10 @@ export class DeleteWorker {
     });
 
     if (result.isConfirmed) {
+
       try {
-        await this.userService.deactivate(this.id_users);
+
+        await this.userService.deleteUser(this.selectedUser()!.id_users);
 
         await Swal.fire({
           title: 'Deactivated!',
@@ -69,16 +90,21 @@ export class DeleteWorker {
           confirmButtonText: 'Ok',
         });
 
-        // 3. Redirigir al panel de administración (importante tras borrar)
         this.router.navigate(['/dashboard_admin']);
+
       } catch (error) {
+
         Swal.fire({
           title: 'Mistake!',
           text: 'There was a problem deactivating the worker',
           icon: 'error',
           confirmButtonText: 'Ok',
         });
+
       }
+
     }
+
   }
+
 }
